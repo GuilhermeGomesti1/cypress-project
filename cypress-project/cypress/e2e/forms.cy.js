cb51c60229597ea7b63a2570a6d2231e0d53a28e
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-09 16:40:14 -0300

    tests

---
+++ cypress-project/cypress/e2e/forms.cy.js
@@ -0,0 +1,36 @@
+describe("form tests", () => {
+  beforeEach(() => {
+    cy.visit("/forms");
+  });
+  it("Test subscribe form", () => {
+    cy.contains(/testing forms/i);
+    cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
+    cy.get("@subscribe-input").type("guilherme.gomes.ti1@gmail.com");
+    cy.contains(/Successfully subbed: guilherme.gomes.ti1@gmail.com!/i).should(
+      "not.exist"
+    );
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/Successfully subbed: guilherme.gomes.ti1@gmail.com!/i).should(
+      "exist"
+    );
+    cy.wait(3000);
+    cy.contains(/Successfully subbed: guilherme.gomes.ti1@gmail.com!/i).should(
+      "not.exist"
+    );
+
+    cy.get("@subscribe-input").type("guilherme.gomes.ti1gmail.io");
+    cy.contains(/Invalid email: guilherme.gomes.ti1gmail.io!/i).should(
+      "not.exist"
+    );
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/Invalid email: guilherme.gomes.ti1gmail.io!/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/Invalid email: guilherme.gomes.ti1gmail.io!/i).should(
+      "not.exist"
+    );
+
+    cy.contains(/fail!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/fail!/i).should("exist");
+  });
+});
--- cypress-project/cypress/e2e/fundamentals.cy.js
+++ cypress-project/cypress/e2e/fundamentals.cy.js
@@ -1,16 +1,22 @@
 describe("Fundamentals test", () => {
-  it("Contains correct header text", () => {
+  beforeEach(() => {
     cy.visit("/fundamentals");
-    cy.get('[data-test="fundamentals-header"]').contains(
-      /Testing Fundamentals/i
-    );
+  });
+  it("Contains correct header text", () => {
+    cy.getDataTest("fundamentals-header").contains(/Testing Fundamentals/i);
   });
 
   it("Accordion works correctly", () => {
-    cy.visit("/fundamentals");
     cy.contains(/Your tests will exist in a describe block/i).should(
       "not.be.visible"
     );
-    cy.get('[data-test="accordion-item-1"]').click().pause();
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
   });
 });
